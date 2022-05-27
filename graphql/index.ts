666e471d61b53f84f90484bdee6a2adfe5ad926f
Vlad Voznitsa <voznitsa98@gmail.com>
2022-05-27 10:39:01 +0600

    fix

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const graphClient = new ApolloClient({
-    uri: 'https://ec2-52-37-206-149.us-west-2.compute.amazonaws.com:4000/graphql',
+    uri: 'http://ec2-52-37-206-149.us-west-2.compute.amazonaws.com:4000/graphql',
     cache: new InMemoryCache()
 })
