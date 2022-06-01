7ba3821dfe20295a2741adc2125e5900ec0d52ff
Vlad Voznitsa <voznitsa98@gmail.com>
2022-06-01 19:12:53 +0600

    Changed config

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ export const URIs = {
     // apiURI: "http://localhost:4000",
 
     //HEROKU:
-    apiURI: "http://orbeem-api.herokuapp.com",
+    apiURI: "https://orbeem-api.herokuapp.com",
 
     
     subgraphURI: "https://api.thegraph.com/subgraphs/name/vladknd/orbeem"
