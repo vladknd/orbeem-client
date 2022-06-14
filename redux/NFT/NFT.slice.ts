88ed5333ae6fd42bb4c6077082b43540fb010b7d
Vlad Voznitsa <voznitsa98@gmail.com>
2022-06-15 03:59:45 +0600

    Fixed NFT.slice

--- redux/NFT/NFT.slice.ts
+++ redux/NFT/NFT.slice.ts
@@ -14,7 +14,6 @@ const nftSlice = createSlice({
     name: "NFT",
     initialState: initState,
     reducers: {
-        // fetchNFT: fetchNFTReducer
         nftFetching(state: INFTInitState) {
             state.loading = true
         },
@@ -30,23 +29,23 @@ const nftSlice = createSlice({
             state.upgrading = !state.upgrading
         },
         nftIncrPower(state: INFTInitState) {
-            if(state.nft){
-                const total = state.nft.power + state.nft.durability 
-                const baseTotal = state.nft.basePower + state.nft.baseDurability
-                const allowance = state.nft.level*4 + baseTotal
+            if(state.item){
+                const total = state.item.power + state.item.durability 
+                const baseTotal = state.item.basePower + state.item.baseDurability
+                const allowance = state.item.level*4 + baseTotal
 
-                if(total < allowance) state.nft.power = (state.nft.power+1)
+                if(total < allowance) state.item.power = (state.item.power+1)
             } 
         },
         nftIncrDurability(state: INFTInitState) {
-            if(state.nft){
-                const total = state.nft.power + state.nft.durability 
-                const baseTotal = state.nft.basePower + state.nft.baseDurability
-                const allowance = state.nft.level*4 + baseTotal
+            if(state.item){
+                const total = state.item.power + state.item.durability 
+                const baseTotal = state.item.basePower + state.item.baseDurability
+                const allowance = state.item.level*4 + baseTotal
                 console.log("REDUCER's alllowance", allowance);
                 console.log("REDUCER's total", total)
                 
-                if(total < allowance) state.nft.durability = (state.nft.durability+1)
+                if(total < allowance) state.item.durability = (state.item.durability+1)
             } 
         },
 
